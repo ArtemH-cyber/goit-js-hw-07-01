752d12fecf26a5a82c17bb0c008931a823cab0dc
Artem Halchyk <art234m@gmail.com>
2021-09-14 15:16:28 +0300

    finish

---
+++ task-01.js
@@ -0,0 +1,5 @@
+// const categoriesById = document.getElementById('categories')
+// console.log(categoriesById)
+
+const categoriesByTag = document.querySelectorA('h1')
+console.log(categoriesByTag)
